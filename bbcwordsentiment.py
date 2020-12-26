51d34a0249b6dcf241ba3814f7ea5e5e659997b3
Caspar <zcaspar@gmail.com>
2020-12-26 08:08:28 +0000

    Add Telegraph and NYT

--- bbcwordsentiment.py
+++ bbcwordsentiment.py
@@ -48,13 +48,21 @@ all_headlines = get_headlines(soup)
 
 headline_list = []
 for i in all_headlines:
-    headline_list.append(i.text)
+    if i.text not in headline_list and (i.text != ''):
+        headline_list.append(i.text)
+        
+# Remove BBC links
+headline_list = headline_list[5:]
 
 
 # Remove stop words and punctuation and put remaining words in sentiment_words
 
+# Create a list of individual words in headline and put into this:
+individualized_headline_words = []
+
 for headline in headline_list:
     doc = nlp(headline)
+    individualized_headline_words.append(headline.split())
     sentiment_words.append([token.lemma_ for token in doc if not token.is_stop if not token.lemma_ in stop_words if token.is_punct == False])
 
 # Create a large list for all words in all headlines
@@ -116,6 +124,84 @@ def json_to_dic(file_name):
         this_dict = json.load(f)
     return this_dict
    
-result = round((sum(score_dict)/len(score_dict)*10),2)
+result = round((sum(score_dict)/len(score_dict)),2)
+
+# Get polarity score for individual headline
+
+#NEW
+# Add headline score to a list
+# last_serial contains the last serial no. in the dictionary
+# Opening JSON file
+
+
+with open('/home/caspar/Documents/Data Science/bbc_sentiment_analysis/headline_scores.json') as json_file: 
+    data = json.load(json_file) 
+
+counter = 0
+for i in reversed(data):
+    while counter < 1:
+        last_serial = int(i)
+        counter += 1
+
+
+headline_score = 0
+#serial_no = 0
+new_headline_list = []
+headline_dict = {} #json_to_dic('/home/caspar/Documents/Data Science/bbc_sentiment_analysis/headline_scores.json')
+
+
+#start the count at the end of the last dictionary
+headline_counter = int(last_serial)
+# Get the current date and time
+
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+ 
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+for headline in headline_list:
+    # Check if headline is already in the headline_list
+    if headline not in new_headline_list:
+        # Add headline score to a list
+        single_word_sentiment = []
+        for bbc_word in headline.split():
+            if bbc_word in polarity_dictionary:
+                single_word_sentiment.append(polarity_dictionary[bbc_word])
+        if len(single_word_sentiment) > 0:
+            headline_score = round(sum(single_word_sentiment)/len(single_word_sentiment),2)
+        else:
+            headline_score = 0
+        new_headline_list.append((headline_list[headline_counter-int(last_serial)],headline_score))
+        headline_dict[headline_counter+1] = (new_headline_list[headline_counter-int(last_serial)],dt_string,headline_score,0)
+        headline_counter += 1
+        last_serial += 1
+'''
+OLD
+# Add headline score to a list
+headline_score = 0
+new_headline_list = []
+headline_dict = {}
+headline_counter = 0
+for headline in individualized_headline_words:
+    # Add headline to a list
+
+    # Add headline score to a list
+    single_word_sentiment = []
+    for bbc_word in headline:
+        if bbc_word in polarity_dictionary:
+            single_word_sentiment.append(polarity_dictionary[bbc_word])
+    if len(single_word_sentiment) > 0:
+        headline_score = round(sum(single_word_sentiment)/len(single_word_sentiment),2)
+    else:
+        headline_score = 0
+    new_headline_list.append((headline,headline_score))
+    headline_dict[headline_counter] = (headline,headline_score)
+    headline_counter += 1
+'''
+dict_to_json(headline_dict,'/home/caspar/Documents/Data Science/bbc_sentiment_analysis/headline_scores.json')
 
 print(result)
